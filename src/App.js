664d0480a266cbedfc9f568132e55cb68be10f43
Dan Shrout <danshrout@yahoo.com>
2020-06-28 19:20:50 -0500

    deploy app to firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
+// import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
@@ -49,7 +49,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Contact data={this.state.resumeData.main}/>
+        {/* <Contact data={this.state.resumeData.main}/> */}
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
